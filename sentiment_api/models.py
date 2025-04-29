315813a51fe28ad016852270a60dbfaa75a48b96
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-04-29 12:23:20 +0530

    Update models.py
    pip install torch

--- sentiment_api/models.py
+++ sentiment_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import torch.nn as nn
 
 # Create your models here.
+class SentimentModel(nn.Module):
+    def __init__(self, vocab_size, embed_dim, hidden_dim, output_dim):
+        super(SentimentModel, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.rnn = nn.GRU(embed_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        output, hidden = self.rnn(embedded)
+        return self.fc(hidden.squeeze(0))
